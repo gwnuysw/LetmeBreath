2fab10fedfa914ef45f90db6f3ab5de89ff78f7a
seokwon <swlee1014@gmail.com>
2019-05-12 16:01:54 +0900

    pw using bcrypt

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ let router = express.Router();
 let users = require('../schemas/user');
 let { isLoggedIn, isNotLoggedIn } = require('./logincheck');
 let passport = require('passport');
+let bcrypt = require('bcrypt');
 const LocalStrategy = require('passport-local').Strategy;
+
 /* GET users listing. */
 router.get('/joinpage', isNotLoggedIn, function(req, res, next) {
   res.render('joinpage');
@@ -20,15 +22,22 @@ router.post('/join', async function(req,res,next){
   let user = {
     id: req.body.id,
     pw: req.body.pw,
+    pwconfirm: req.body.pwconfirm,
     age: req.body.age,
     weight: req.body.weight
   };
   let getuser = await users.findOne({id:user.id});
   console.log(getuser);
   if(getuser == undefined){
-    let newUser = new users(user);
-    await newUser.save();
-    res.send('회원가입 되었습니다.');
+    if(user.pw != user.pwconfirm){
+      res.send('비밀번호를 확인해 주세요');
+    }
+    else{
+      user.pw = await bcrypt.hash(user.pw, 12);
+      let newUser = new users(user);
+      await newUser.save();
+      res.redirect('/users/loginpage');
+    }
   }
   else{
     res.send('id가 이미 존재합니다.');
