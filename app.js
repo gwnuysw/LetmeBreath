f92c8992826b48fc00ace39b231a2f9cc2ab2f33
seokwon <swlee1014@gmail.com>
2019-05-26 17:28:22 +0900

    rtouer

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var stationRouter = require('./routes/station');
 var pinedustRouter = require('./routes/finedust');
+var updateRouter = require('./routes/update');
 var askdust = require('./bin/pinedust');
 var app = express();
 
@@ -45,6 +46,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/station',stationRouter);
 app.use('/finedust',pinedustRouter);
+app.use('/update',updateRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
